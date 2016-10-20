6b3d9209ebcbad151faddfa9e5538b6ef4bb5d30
Pamasoli19 <jpmscr19@gmail.com>
2016-10-20 11:39:15 -0600

    Establecimiento de sinicronizaciòn de Proyecto Practico de un Paint en Android Studio, Para curso Sistemas Operativos 2

---
+++ app/src/main/java/com/jpmscr19gmail/paint_4_os2/Lienzo.java
@@ -0,0 +1,16 @@
+package com.jpmscr19gmail.paint_4_os2;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Pablo on 22/10/2016.
+ */
+
+public class Lienzo extends View{
+    public Lienzo(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
