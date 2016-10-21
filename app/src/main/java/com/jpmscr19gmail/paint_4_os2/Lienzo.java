85ad791c16018f495a51e3c07a45efc45cb6e887
Pamasoli19 <jpmscr19@gmail.com>
2016-10-21 13:47:48 -0600

    Desarrollando variables

--- app/src/main/java/com/jpmscr19gmail/paint_4_os2/Lienzo.java
+++ app/src/main/java/com/jpmscr19gmail/paint_4_os2/Lienzo.java
@@ -2,6 +2,10 @@ package com.jpmscr19gmail.paint_4_os2;
 
 
         import android.content.Context;
+        import android.graphics.Bitmap;
+        import android.graphics.Canvas;
+        import android.graphics.Paint;
+        import android.graphics.Path;
         import android.util.AttributeSet;
         import android.view.View;
 
@@ -10,12 +14,35 @@ package com.jpmscr19gmail.paint_4_os2;
  */
 
 public class Lienzo extends View{
+
+    private Path drawPath;
+
+    private Paint drawPaint, canvasPaint;
+
+    private int paintColor = 0xFFFF0000;
+
+    private Canvas drawCanvas;
+
+    private Bitmap canvasBitmap;
+
     public Lienzo(Context context, AttributeSet attrs) {
+
         super(context, attrs);
     }
 
     private void setupDrawing(){
 
-    }
+        drawPath = new Path();
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(20);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
 
+
+    }
 }
+
