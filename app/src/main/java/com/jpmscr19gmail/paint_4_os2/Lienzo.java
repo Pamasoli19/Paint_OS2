8bed72409bd74a01e86e2a8812e7b9cd89fea501
Pamasoli19 <jpmscr19@gmail.com>
2016-10-21 00:03:47 -0600

    Aplicando relaciones entre formularios para acciones de botones superiores

--- app/src/main/java/com/jpmscr19gmail/paint_4_os2/Lienzo.java
+++ app/src/main/java/com/jpmscr19gmail/paint_4_os2/Lienzo.java
@@ -4,16 +4,19 @@ package com.jpmscr19gmail.paint_4_os2;
         import android.content.Context;
         import android.graphics.Bitmap;
         import android.graphics.Canvas;
+        import android.graphics.Color;
         import android.graphics.Paint;
         import android.graphics.Path;
         import android.util.AttributeSet;
+        import android.util.TypedValue;
+        import android.view.MotionEvent;
         import android.view.View;
 
 /**
  * Created by Pablo on 22/10/2016.
  */
 
-public class Lienzo extends View{
+public class Lienzo extends View {
 
     private Path drawPath;
 
@@ -25,24 +28,74 @@ public class Lienzo extends View{
 
     private Bitmap canvasBitmap;
 
-    public Lienzo(Context context, AttributeSet attrs) {
+    float TamanyoPunto;
 
+    public Lienzo(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setupDrawing();
     }
 
-    private void setupDrawing(){
+    private void setupDrawing() {
 
         drawPath = new Path();
         drawPaint = new Paint();
         drawPaint.setColor(paintColor);
         drawPaint.setAntiAlias(true);
-        drawPaint.setStrokeWidth(20);
+
+        setsize_punto(20);
+
+        drawPaint.setStrokeWidth(TamanyoPunto);
         drawPaint.setStyle(Paint.Style.STROKE);
         drawPaint.setStrokeJoin(Paint.Join.ROUND);
         drawPaint.setStrokeCap(Paint.Cap.ROUND);
         canvasPaint = new Paint(Paint.DITHER_FLAG);
 
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                drawPath.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                drawPath.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawPath.lineTo(touchX, touchY);
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+            default:
+                return false;
+        }
+        //Para repintar
+        invalidate();
+        return true;
+    }
+    public void setColor(String newColor){
+        invalidate();
+        paintColor = Color.parseColor(newColor);
+        drawPaint.setColor(paintColor);
+    }
+
+    public void setsize_punto(float nuevoTamanyo){
+        float pixel =TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,nuevoTamanyo, getResources().getDisplayMetrics());
+        TamanyoPunto=pixel;
+        drawPaint.setStrokeWidth(TamanyoPunto);
 
     }
 }
-
