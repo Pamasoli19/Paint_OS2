4dd64fb4d1dae5dec727ed2af15c657db08059f2
Pamasoli19 <jpmscr19@gmail.com>
2016-10-22 11:23:23 -0600

    Continuaciòn de programaciòn de los eñementos y propiedadees de los objetos y eventos del paint

--- app/src/main/java/com/jpmscr19gmail/paint_4_os2/Lienzo.java
+++ app/src/main/java/com/jpmscr19gmail/paint_4_os2/Lienzo.java
@@ -1,9 +1,9 @@
 package com.jpmscr19gmail.paint_4_os2;
 
 
-import android.content.Context;
-import android.util.AttributeSet;
-import android.view.View;
+        import android.content.Context;
+        import android.util.AttributeSet;
+        import android.view.View;
 
 /**
  * Created by Pablo on 22/10/2016.
